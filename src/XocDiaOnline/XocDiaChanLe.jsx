8ba20ac24d5a860eb98682739946eab4641f207e
Tuan Bach <tuan.bach@infonam.com>
2022-04-12 17:52:48 +0700

    update css Final Game Xoc Dia

--- src/XocDiaOnline/XocDiaChanLe.jsx
+++ src/XocDiaOnline/XocDiaChanLe.jsx
@@ -7,10 +7,15 @@ export default function XocDiaChanLe() {
   return (
     <div className="row text-center container m-auto">
       <div className="col-3">
-        <button className='btn btn-dark' style={{
+        <button className='btn btn-dark' id='moBat' style={{
           margin: 'auto',
+          display:'none',
           margin: '50%'
         }} onClick={() =>{
+          document.getElementById('moBat').style.display='none';
+          document.getElementById('bat').classList.add('hieuUngmoBat');
+          document.getElementById('upBat').style.display = 'block';
+          document.getElementById('bat').style.display = 'none';
           dispatch({
             type: 'XOC_DIA'
           })
@@ -26,12 +31,15 @@ export default function XocDiaChanLe() {
           <QuanCuoc></QuanCuoc>
           <img src="./img/bat.png" className='image' id='bat' alt="" style={{
             cursor: 'pointer'
+          }} onClick={() =>{
+            document.getElementById('moBat').style.display='block';
           }} />
         </div>
       </div>
-      <div className="col-3">
-        <button className='btn btn-dark' onClick={()=>{
+      <div className="col-3" id='upBat' >
+        <button className='btn btn-dark'onClick={()=>{
           document.getElementById('bat').style.display = 'block';
+          document.getElementById('upBat').style.display = 'none';
         }} style={{
           margin: 'auto',
           margin: '50%'
